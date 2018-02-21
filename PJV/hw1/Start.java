9fb0b837375584376ec5e1caddcb3fd050314968
palomikula <36712947+palomikula@users.noreply.github.com>
2018-02-21 22:12:24 +0100

    Add files via upload

---
+++ PJV/hw1/Start.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author palo
+ */
+
+
+public class Start {
+
+   public static void main(String[] args) {
+      Lab01 lab = new Lab01();
+      lab.start(args);
+   }
+
+}
